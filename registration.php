6a2641da86f184006857ff9304fa40f27b7aa03a
Sadiaema009 <raisasadia7@gmail.com>
2021-11-14 10:45:22 +0600

    done

---
+++ registration.php
@@ -0,0 +1,59 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="design.css">
+</head>
+<body>
+<div  class="Div4">
+<h1>ABC Management System</h5>
+<h4 >we create future<h4>
+</div>
+<div  class="Div3">
+<h5>Home        About us        Shop</h5>
+</div>
+<div  class="Div1">
+<h1>Registration Form</h1>
+</div>
+
+
+<form action=" ">
+<div class="Div2">
+<label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="Age">Age:</label>
+  <input type="text" id="age" name="Age"><br><br>
+  <label for="designation">Designation:</label>
+  <input type="radio" id="d1" name="D11" value="Junior programmer">
+  <label for="d1">Junior Programmer</label>
+  <input type="radio" id="d2" name="D1" value="Senior Programmer">
+  <label for="d2">Senior Programmer</label>
+  <input type="radio" id="d3" name="D1" value="Project lead">
+  <label for="d3">Project Lead</label><br>
+  
+  <label for="language">preffered Language:</label>
+  <input type="radio" id="l1" name="L1" value="JAVA">
+  <label for="l1">JAVA</label>  
+  <input type="radio" id="l2" name="L1" value="PHP">
+  <label for="l2">PHP</label> 
+  <input type="radio" id="l3" name="L1" value="C++">
+  <label for="l3">C++</label><br><br>
+  <label for="email">E-mail:</label>
+  <input type="text" id="E-mail" name="email"><br><br>
+  <label for="pass">Password:</label>
+  <input type="text" id="Pass" name="pass"><br><br>
+  <input type="submit" class="btn" value="Submit">
+  <input type="submit" class="btn" value="reset">
+
+</div>
+  
+</form>
+
+
+
+</body>
+</html>
